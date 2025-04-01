8f699def81d9c0b22448f7391449830af38e8269
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-04-01 08:26:51 -0300

    Desenvolvimento da interface para salvar os dados e instalação do Async Storage

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const[nomeProduto,setNomeProduto]=useState("")
+  const[precoProduto,setPrecoProduto]=useState()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Cadastro</Text>
+      <TextInput 
+        placeholder='Digite o nome do produto'
+        style={styles.input}
+        value={nomeProduto}
+        onChangeText={(value)=>setNomeProduto(value)}
+      />
+      <TextInput 
+        placeholder='Digite o preço do produto'
+        style={styles.input}
+        keyboardType='numeric'
+        value={precoProduto}
+        onChangeText={(value)=>setPrecoProduto(value)}
+      />
+      <TouchableOpacity style={styles.btn}>
+        <Text style={{color:"white"}}>CADASTRAR</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +36,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderWidth:1,
+    height:50,
+    width:300,
+    borderRadius:15,
+    marginTop:10
+  },
+  btn:{
+    backgroundColor:"blue",
+    borderWidth:1,
+    height:50,
+    width:300,
+    borderRadius:15,
+    justifyContent:"center",
+    alignItems:"center",
+    marginTop:10
+  }
 });
