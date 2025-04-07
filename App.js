cd2621e7c5f059ae9bbcc51b824820ddb0f58b38
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-04-07 09:44:09 -0300

    CRUD desenvolvido, realizado o botão e botão editar, useEffect

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { useState,useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList,Keyboard } from 'react-native';
 import { TextInputMask } from 'react-native-masked-text';
 
 export default function App() {
   const [nomeProduto, setNomeProduto] = useState("")
   const [precoProduto, setPrecoProduto] = useState()
   const [listaProdutos,setListaProdutos]=useState([])
+  const [produtoEditado,setProdutoEditado]=useState(null)
+
+  useEffect(()=>{
+    buscarDados()
+  },[])
 
   async function salvar() {
+    Keyboard.dismiss()
     let produtos = []
 
     if(await AsyncStorage.getItem("PRODUTOS")!=null){
       produtos = JSON.parse(await AsyncStorage.getItem("PRODUTOS"))
     }
 
-    produtos.push({nome:nomeProduto,preco:precoProduto})
+    if(produtoEditado){
+      //Atualizar o produto existente
+      produtos[produtoEditado.index] = {nome:nomeProduto,preco:precoProduto}
+    }else{
+      produtos.push({nome:nomeProduto,preco:precoProduto})
+    }
+
+    
 
     //Salvado os dados no Async Storage
     await AsyncStorage.setItem("PRODUTOS",JSON.stringify(produtos))
 
-    alert("PRODUTO SALVO")
+    alert(produtoEditado?"PRODUTO ATUALIZADO":"PRODUTO CADASTRADO")
+
+    setProdutoEditado(null)
+    
+
+    //Limpando formulário
+    setNomeProduto('')
+    setPrecoProduto('')
 
     buscarDados()
   }
@@ -28,6 +48,24 @@ export default function App() {
   async function buscarDados() {
     const p = await AsyncStorage.getItem("PRODUTOS")
     setListaProdutos(JSON.parse(p))
+  }
+
+  async function deletarProduto(index){
+    //console.log(index) //Verificando o index que será passado
+    const tempDados = listaProdutos
+    const dados = tempDados.filter((item,ind)=>{
+       return ind!==index
+    })
+
+    setListaProdutos(dados)//Atualizado o estado listaProdutos
+    await AsyncStorage.setItem("PRODUTOS",JSON.stringify(dados)) // Atualiza o banco sem o item excluido
+  }
+
+  function editarProduto(index){
+    const produto = listaProdutos[index]
+    setNomeProduto(produto.nome)
+    setPrecoProduto(produto.preco)
+    setProdutoEditado({index})//Definindo qual produto a ser editado
   }
 
   return (
@@ -47,7 +85,7 @@ export default function App() {
         onChangeText={(value) => setPrecoProduto(value)}
       />
       <TouchableOpacity style={styles.btn} onPress={salvar}>
-        <Text style={{ color: "white" }}>CADASTRAR</Text>
+        <Text style={{ color: "white" }}>{produtoEditado?"ATUALIZAR":"CADASTRAR"}</Text>
       </TouchableOpacity>
 
       {/* {Botão para buscarDados} */}
@@ -58,10 +96,29 @@ export default function App() {
       <FlatList 
         data={listaProdutos}
         renderItem={({item,index})=>{
+          if(!item || !item.nome ) return null;// Garante que a FlatList só renderize se as propriedades for diferenete nulo.
           return(
             <View style={styles.listFlat}>
               <View> 
                 <Text>Nome:{item.nome} - Preco:{item.preco}</Text>
+              </View>
+
+              <View style={{flexDirection:"row"}}>
+                {/* Botão Excluir */}
+                <TouchableOpacity 
+                  style={styles.btnExcluir} 
+                  onPress={()=>deletarProduto(index)}
+                >
+                  <Text>Excluir</Text>
+                </TouchableOpacity>
+
+                {/* Botão Editar */}
+                <TouchableOpacity 
+                  style={styles.btnEditar} 
+                  onPress={()=>editarProduto(index)}
+                >
+                  <Text>Editar</Text>
+                </TouchableOpacity>
               </View>
             </View>
 
@@ -105,5 +162,20 @@ const styles = StyleSheet.create({
     justifyContent:"center",
     alignItems:"center",
     marginVertical:3
+  },
+  btnExcluir:{
+    width:100,
+    backgroundColor:"red",
+    borderRadius:15,
+    alignItems:"center",
+    marginTop:7
+  },
+  btnEditar:{
+    width:100,
+    backgroundColor:"orange",
+    borderRadius:15,
+    alignItems:"center",
+    marginTop:7,
+    marginLeft:5
   }
 });
